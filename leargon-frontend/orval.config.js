cd257ec56d6fe0e94fa31eafba222a6496bcff59
Christoph Schütz <chris.ray.rayne@gmail.com>
2026-02-19 09:02:32 +0100

    - Bumped Minor versions

--- leargon-frontend/orval.config.js
+++ leargon-frontend/orval.config.js
@@ -8,13 +8,12 @@ export default defineConfig({
             target: './src/api/generated/endpoints.ts',
             schemas: './src/api/generated/model',
             client: 'react-query',
-            baseUrl: 'http://localhost:8081',
             mode: 'tags-split',
             prettier: true,
             override: {
                 mutator: {
                     path: './src/api/customAxios.ts',
-                    names: 'customAxios',
+                    name: 'customAxios',
                 },
                 query: {
                     useQuery: true,
